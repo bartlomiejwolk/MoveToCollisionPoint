298e599cc6fcc6d04a62a38a17c739e0df8d1b66
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-11 17:00:04 +0200

    Initial commit

---
+++ MoveToPoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OneDayGame {
+
+    public class MoveToPoint : MonoBehaviour {
+
+        /// Offset hit position forward/backward.
+        [SerializeField]
+        private float _offset;
+
+        [SerializeField]
+        private Transform targetTransform;
+
+        public void MoveToHitPoint(Vector3 hitPoint) {
+
+                targetTransform.position = hitPoint
+                    + (hitPoint- targetTransform.position).normalized * _offset;
+            }
+        }
+}
